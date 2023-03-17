f29718e1536f83d505bfa54d341a77a584e3b3d9
Gary Hooks <garyhooks@gmail.com>
2023-03-17 17:28:42 +0000

    Create main.py

---
+++ main.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+# Author: Gary Hooks
+# Date: March 2023
+# Licence: GPLv3
+
+import requests
+import json
+import sys
+from colorama import Fore, Style
+
+def main():
+    api_key = "1097a49c8d3781b23305f465efa18c7600db5d0e851d3bdd310d63a698cbf2a04056a9c2d1ade9ba"
+    url = 'https://api.abuseipdb.com/api/v2/check'
+
+    ## Minimum "confidence score" to indicate dangerous IP, 100 is the most dangerous
+    danger_threshold = 50
+
+    ## How many days to search over. Min=1, Default=30, Max=365
+    max_days = 90
+
+    ip_list = open("ips.txt", "r")
+    bad_ips = []
+    for line in ip_list:
+        ip = line.strip()
+
+        querystring = {
+            'ipAddress': ip,
+            'maxAgeInDays': max_days
+        }
+
+        headers = {
+            'Accept': 'application/json',
+            'Key': api_key
+        }
+
+        response = requests.request(method='GET', url=url, headers=headers, params=querystring)
+        decodedResponse = json.loads(response.text)
+
+        output = (ip + ": Confidence Score: "
+        + str(decodedResponse["data"]["abuseConfidenceScore"]) + " > "
+        + str(decodedResponse["data"]["totalReports"]) + " total reports by "
+        + str(decodedResponse["data"]["numDistinctUsers"]) + " in the last "
+        + str(max_days) + " days")
+
+        if decodedResponse["data"]["abuseConfidenceScore"] > danger_threshold:
+            print(Fore.RED + output)
+            bad_ips.append(ip)
+        else:
+            print(Fore.GREEN + output)
+
+    print(Fore.RED + "\n[+] All IPs over threshold of " + str(danger_threshold) + " confidence score are listed below:")
+    print("----------------------------------------------------------------------")
+    for ip in bad_ips:
+        print(Fore.RED + ip)
+
+    print(Style.RESET_ALL + "\n")
+
+if __name__ == "__main__":
+    main()
