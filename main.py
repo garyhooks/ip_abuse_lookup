7f83d1612c0c8a725c1fa50827a5dec8b970208b
Gary Hooks <garyhooks@gmail.com>
2023-03-28 13:37:32 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,60 +2,132 @@
 # Author: Gary Hooks
 # Date: March 2023
 # Licence: GPLv3
+# Version 2.0
 
 import requests
 import json
 import sys
 from colorama import Fore, Style
+from prettytable import PrettyTable
 
-def main():
-    
+def ipqualityscore(danger_threshold, ip):
+    api_key = "YOUR API KEY"
+    url = "https://ipqualityscore.com/api/json/ip/" + api_key + "/" + ip + "?strictness=0&allow_public_access_points=true&fast=true&lighter_penalties=true&mobile=true"
+
+    response = requests.request(method="GET", url=url)
+    decodedResponse = json.loads(response.text)
+
+    if decodedResponse["fraud_score"] >= danger_threshold:
+        return True
+    else:
+        return False
+
+def scamalytics(danger_threshold, ip):
+    api_key = "YOUR API KEY"
+    url = "https://api11.scamalytics.com/garyhooks/?key=" + api_key + "&ip=" + ip
+
+    response = requests.request(method="GET", url=url)
+    decodedResponse = json.loads(response.text)
+
+    if decodedResponse["credits"]["remaining"] < 25:
+        print("[!] Running Low on credits, less than 25 remaining")
+
+
+    if decodedResponse["score"] >= danger_threshold:
+        return True
+    else:
+        return False
+
+def abuseipdb(danger_threshold, max_days, ip):
     ## Register here to obtain your own API key: https://www.abuseipdb.com/login
-    api_key = "PUT YOUR KEY HERE"
+    api_key = "YOUR API KEY"
     url = 'https://api.abuseipdb.com/api/v2/check'
 
-    ## Minimum "confidence score" to indicate dangerous IP, 100 is the most dangerous
-    danger_threshold = 50
+    querystring = {
+        'ipAddress': ip,
+        'maxAgeInDays': max_days
+    }
 
-    ## How many days to search over. Min=1, Default=30, Max=365
+    headers = {
+        'Accept': 'application/json',
+        'Key': api_key
+    }
+
+    response = requests.request(method='GET', url=url, headers=headers, params=querystring)
+    decodedResponse = json.loads(response.text)
+
+    if decodedResponse["data"]["abuseConfidenceScore"] >= danger_threshold:
+        return True
+    else:
+        return False
+
+
+def output_results(bad_ips):
+        print(Fore.RED + "\n[+] Tabular results of Bad IPs:")
+        print("-----------------------------------------------------------------------")
+        print(Style.RESET_ALL)
+
+        table = PrettyTable()
+        table.field_names = ["IP", "AbuseIPDB", "Scamalytics", "ipqualityscore"]
+        table.align = "l"
+        for record in bad_ips.values():
+            table.add_row(
+                [
+                    record["ip"],
+                    record["abuseipdb"],
+                    record["scamalytics"],
+                    record["ipqualityscore"]
+                ]
+            )
+
+        print(table)
+
+        print(Fore.RED + "\n[+] CSV List:")
+        print("-----------------------------------------------------------------------")
+        print(Style.RESET_ALL)
+
+        print("ip,abuseipdb,abuseipdb-URL, scamalytics, scamalytics-URL, ipqualityscore, ipqualityscore-URL, Google")
+        for record in bad_ips.values():
+            print(record["ip"] + "," +
+                  str(record["abuseipdb"]) + "," +
+                  "https://www.abuseipdb.com/check/" + record["ip"] + "," +
+                  str(record["scamalytics"]) + "," +
+                  "https://scamalytics.com/ip/" + record["ip"] + "," +
+                      str(record["ipqualityscore"]) + "," +
+                  "https://www.ipqualityscore.com/free-ip-lookup-proxy-vpn-test/lookup/" + record["ip"] + "," +
+                    "https://www.google.com/search?q=" + record["ip"]
+                    )
+
+        print(Style.RESET_ALL)
+
+        print(Fore.RED + "\n[+] Plain Text list of bad IPs shown below:")
+        print("-----------------------------------------------------------------------")
+        print(Style.RESET_ALL)
+
+        for record in bad_ips.values():
+            print(record["ip"])
+
+        print(Style.RESET_ALL + "\n")
+
+
+if __name__ == "__main__":
+    danger_threshold = 50
+    ## Abuse IPDB: How many days to search over. Min=1, Default=30, Max=365
     max_days = 90
 
+    bad_ips = {}
+    counter = 0
+
     ip_list = open("ips.txt", "r")
-    bad_ips = []
     for line in ip_list:
         ip = line.strip()
 
-        querystring = {
-            'ipAddress': ip,
-            'maxAgeInDays': max_days
-        }
+        bad_ips[counter] = { "ip" : ip,
+                        "abuseipdb" : abuseipdb(danger_threshold, max_days, ip),
+                        "scamalytics" : scamalytics(danger_threshold, ip),
+                        "ipqualityscore" : ipqualityscore(danger_threshold, ip)
+                        }
+        counter += 1
 
-        headers = {
-            'Accept': 'application/json',
-            'Key': api_key
-        }
 
-        response = requests.request(method='GET', url=url, headers=headers, params=querystring)
-        decodedResponse = json.loads(response.text)
-
-        output = (ip + ": Confidence Score: "
-        + str(decodedResponse["data"]["abuseConfidenceScore"]) + " > "
-        + str(decodedResponse["data"]["totalReports"]) + " total reports by "
-        + str(decodedResponse["data"]["numDistinctUsers"]) + " in the last "
-        + str(max_days) + " days")
-
-        if decodedResponse["data"]["abuseConfidenceScore"] > danger_threshold:
-            print(Fore.RED + output)
-            bad_ips.append(ip)
-        else:
-            print(Fore.GREEN + output)
-
-    print(Fore.RED + "\n[+] All IPs over threshold of " + str(danger_threshold) + " confidence score are listed below:")
-    print("----------------------------------------------------------------------")
-    for ip in bad_ips:
-        print(Fore.RED + ip)
-
-    print(Style.RESET_ALL + "\n")
-
-if __name__ == "__main__":
-    main()
+    output_results(bad_ips)
